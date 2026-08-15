8d9ee652761d3e496e3ed05a71c4238ad6d1e561
abeccari <47361308+abeccari@users.noreply.github.com>
2026-08-15 09:14:01 +0200

    Plot lfsr sequence

--- test/lfsr_sequence.py
+++ test/lfsr_sequence.py
@@ -21,15 +21,32 @@ def lfsr_steps(
     return lfsr
 
 if __name__ == "__main__":
+
+    # View wafeform and verify period
+
+    import matplotlib.pyplot as plt
+
     seed = 0x0abba
     lfsr = seed
     period = 0
+    out = []
+
     while True:
+        out.append(lfsr & 1)
         lfsr = lfsr_steps(1, seed=lfsr)
         period += 1
         if lfsr == seed:
             print(f"Actual period: {period}, maximum possible: {2**LFSR_BITS - 1}")
             break
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 3))
+    plot_cycles = min(128, period)
+    ax.step(out[0:plot_cycles], '-')
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Output bit")
+    plt.tight_layout()
+    plt.show()
+    
     
             
 
